0736f4bc4295887a955ee9c5d18dff4fed8bbe6d
wyjeon <wyjeon.info@gmail.com>
2021-03-06 15:39:39 +0900

    React, Redux, redux-actions

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { decrease, increase } from '../modules/count';
+
+function CounterContainer({ number, increase, decrease }) {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+}
+
+const mapStatToProps = state => ({
+  number: state.counter.number,
+});
+
+const mapDispatchToProps = dispatch => ({
+  increase: () => {
+    dispatch(increase());
+  },
+  decrease: () => {
+    dispatch(decrease());
+  },
+});
+
+export default connect(mapStatToProps, mapDispatchToProps)(CounterContainer);
